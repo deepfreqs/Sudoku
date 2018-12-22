99ba15b3a4e25fe2ea65ef4fe31b1766847d3b3b
deepfreqs <deepfrequencies@quantentunnel.de>
2018-12-23 00:36:59 +0100

    test on pavilion

--- src/main/java/org/deepfrequencies/sudoku/domain/SudokuCell.java
+++ src/main/java/org/deepfrequencies/sudoku/domain/SudokuCell.java
@@ -98,6 +98,7 @@ public class SudokuCell {
 			int oldValue = getValue();
 			logger.debug("SudokuPlayground: setHiddenSinglesAsValues: old = " + oldValue + ", newValue = " + newValue);
 			setValue(newValue.get());
+			//recursive trial, did not work out
 			//this.getUniquedNeighbourhood().forEach(SudokuCell::setHiddenSinglesAsValues);
 		}
 		logger.debug("SudokuPlayground: " + this.toString());
